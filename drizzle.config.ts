040ba13cfdc63519573e093315edd91e5366b034
DewinU <umana.dewin@gmail.com>
2024-08-06 00:36:14 -0600

    add src folder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@
 import type { Config } from 'drizzle-kit'
 
 export default {
-  schema: './db/schema.ts',
-  out: './db/migrations',
+  schema: './src/db/schema.ts',
+  out: './src/db/migrations',
   dialect: 'sqlite',
   dbCredentials: {
     url: process.env.DB_URL!,
